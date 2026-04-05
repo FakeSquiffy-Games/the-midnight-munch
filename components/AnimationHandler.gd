a6f5c42788f508a454bc3736cb5655c001f54605
Neyro Caraig <neyrocaraig@gmail.com>
2026-04-05 12:59:14 +0800

    feat: added nametags and floating text

--- components/AnimationHandler.gd
+++ components/AnimationHandler.gd
@@ -11,6 +11,9 @@ var _current_scale_mult: float = 1.0
 func _ready() -> void:
 	_entity = owner as Entity
 	call_deferred("_initialize")
+	
+	## Listen to global boosts so we can spawn "+Speed" text
+	SignalBus.boost_applied.connect(_on_global_boost_applied)
 
 func _initialize() -> void:
 	_stat_manager = _entity.get_node_or_null("StatManager")
@@ -23,15 +26,30 @@ func _initialize() -> void:
 	_last_level = _stat_manager.level
 	_current_scale_mult = _calculate_scale_for_level(_last_level)
 	_apply_scale_multiplier(_current_scale_mult)
+	_update_nametag() # Call once on startup to set the initial text
+
 
 func _process(_delta: float) -> void:
 	if not is_instance_valid(_stat_manager): return
 	
+	## NEW: Counter-flip the nametag so it never reads backwards
+	if is_instance_valid(_entity.model):
+		var label := _entity.model.get_node_or_null("Nametag") as Label
+		if label:
+			label.scale.x = -1.0 if _entity.sync_flip_h else 1.0
+	
 	var current_xp = _stat_manager.current_xp
 	if current_xp != _last_xp:
-		## Flash red if XP was lost
-		if current_xp < _last_xp:
+		var diff: float = current_xp - _last_xp
+		
+		if diff < 0:
 			_flash_red()
+			## Spawn "-XP" text in Red
+			FloatingText.spawn(_entity.get_parent(), _entity.global_position, str(round(diff)) + " XP", Color(1.0, 0.3, 0.3))
+		elif diff > 0:
+			## Spawn "+XP" text in Cyan
+			FloatingText.spawn(_entity.get_parent(), _entity.global_position, "+" + str(round(diff)) + " XP", Color(0.3, 1.0, 1.0))
+			
 		_last_xp = current_xp
 		
 		if not _entity.is_in_group("players"):
@@ -51,6 +69,8 @@ func _on_level_changed(new_level: int) -> void:
 	
 	## Elegant property tweening straight to the Entity
 	tween.tween_property(_entity, "current_scale_mult", target_mult, 0.4)
+	
+	_update_nametag() # Update the nametag when they level up
 
 ## Safely applies scale while honoring the current flip state
 func _apply_scale_multiplier(mult: float) -> void:
@@ -75,3 +95,34 @@ func _calculate_scale_for_level(lvl: int) -> float:
 	else:
 		var clamped: int = min(lvl, 10)
 		return 2.8 + ((clamped - 8) * 0.2)     # Adult: 2.8x to 3.2x
+
+## NEW: Formats and updates the nametag label
+func _update_nametag() -> void:
+	if not is_instance_valid(_entity.model): return
+	
+	var label := _entity.model.get_node_or_null("Nametag") as Label
+	if label == null: return
+	
+	var tier_str := "B"
+	if _stat_manager.tier == 1: tier_str = "T"
+	elif _stat_manager.tier == 2: tier_str = "A"
+	
+	var entity_name := _entity.model.species_name
+	
+	## (Phase 8 will replace entity_name with Username for players)
+	label.text = "(%s) Lvl %d | %s" % [tier_str, _stat_manager.level, entity_name]
+
+## Listens to the global signal and spawns text if THIS entity was the one boosted
+func _on_global_boost_applied(target_path: String, stat: int, amount: float, _duration: float) -> void:
+	if str(_entity.get_path()) != target_path: 
+		return
+		
+	var stat_name := ""
+	match stat:
+		StatManager.StatType.SPEED: stat_name = "Speed"
+		StatManager.StatType.BITE_POWER: stat_name = "Bite"
+		StatManager.StatType.MAX_ENERGY: stat_name = "Energy"
+		_: stat_name = "Boost"
+		
+	var text := "+%s %s!" % [str(amount), stat_name]
+	FloatingText.spawn(_entity.get_parent(), _entity.global_position, text, Color(0.3, 1.0, 0.3)) # Green
